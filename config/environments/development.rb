cfbc4c39d4263944ea54a6c5e068cc881074e206
Chase Snelgrove <Chase@Chases-MacBook-Pro.local>
2014-12-04 12:38:49 -0500

    Updated ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,11 +34,8 @@ Churchos::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "localhost",
-    :port => 25,
-  }
+  config.action_mailer.delivery_method = :sendmail
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
