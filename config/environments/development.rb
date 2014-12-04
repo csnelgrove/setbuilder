ca60134af60cbd13ba2832904b263b197ce14bb0
Chase Snelgrove <Chase@Chases-MacBook-Pro.local>
2014-12-04 12:57:26 -0500

    Updated ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,11 +34,15 @@ Churchos::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-
-  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'chasesnelgrove@gmail.com',
+    password:             'jms2:19!',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
   
   
 end
