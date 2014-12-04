b000f67bf4c1b642445755a5c2cc95a33a3ca9cd
Chase Snelgrove <Chase@Chases-MacBook-Pro.local>
2014-12-04 13:52:17 -0500

    Fixed Host

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,4 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "localhost",  
   :port                 => 25,    
 
+
+
 }
+
+ActionMailer::Base.default_url_options[:host] = "run.church"
